d2accea21b382a9522f34350d5b66ac1bf2adad7
AndrMinakoV <borishasanov591@gmail.com>
2023-09-28 20:27:28 +0300

    added tnt texture

--- src/main/java/net/mecheniy/blastymagic/blocks/BlocksInit.java
+++ src/main/java/net/mecheniy/blastymagic/blocks/BlocksInit.java
@@ -2,6 +2,7 @@ package net.mecheniy.blastymagic.blocks;
 import net.mecheniy.blastymagic.BlastyMagic;
 import net.mecheniy.blastymagic.ItemInit;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
@@ -9,12 +10,16 @@ import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
+import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.Explosion;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.HorizontalDirectionalBlock;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.DirectionProperty;
 import net.minecraft.world.level.levelgen.structure.templatesystem.BlockAgeProcessor;
 import net.minecraft.world.level.material.Material;
 import net.minecraft.world.phys.BlockHitResult;
@@ -49,30 +54,48 @@ public class BlocksInit {
 
 
 public static class ExplosiveBlock extends Block {
-    public ExplosiveBlock(Block.Properties properties) {
-        super(properties);
-    }
+    public static final DirectionProperty FACING = HorizontalDirectionalBlock.FACING;
 
+    public ExplosiveBlock(Block.Properties properties) {
+
+        super(properties);
+        this.registerDefaultState(this.stateDefinition.any().setValue(FACING, Direction.NORTH));
+    }
+    @Override
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
+        builder.add(FACING);
+    }
+    @Override
+    public BlockState getStateForPlacement(BlockPlaceContext context) {
+        return this.defaultBlockState().setValue(FACING, context.getHorizontalDirection().getOpposite());
+    }
     @Override
     public InteractionResult use(BlockState state, Level world, BlockPos pos, Player player, InteractionHand hand,
                                  BlockHitResult hit) {
         ItemStack held = player.getItemInHand(hand);
 
         if (!world.isClientSide() && held.getItem() == Items.GUNPOWDER) {
-            world.explode(player, pos.getX(), pos.getY(), pos.getZ(), 4.0F, true, Explosion.BlockInteraction.DESTROY);
+            world.explode(player, pos.getX(), pos.getY(), pos.getZ(), 10.0F, true, Explosion.BlockInteraction.DESTROY);
             held.shrink(1);
             return InteractionResult.CONSUME;
         }
         return super.use(state, world,pos, player,hand,hit);
     }
+
     @Override
     public void wasExploded(Level world, BlockPos pos, Explosion explosion) {
         world.explode(null, pos.getX(), pos.getY(), pos.getZ(), 4.0F, true,
                 Explosion.BlockInteraction.DESTROY);
         super.wasExploded(world, pos, explosion);
     }
+
 }
     public static final RegistryObject<Block> explosive_block = BLOCKS.register("explosive_block",
             ()-> new ExplosiveBlock(BlockBehaviour.Properties.copy(Blocks.DIRT)));
+
+
+
+
+
 
 }
